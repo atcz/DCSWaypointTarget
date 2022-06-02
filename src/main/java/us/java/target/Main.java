db3a86a6d029f0898f20f039880c5a55d21310d0
atcz <86995996+atcz@users.noreply.github.com>
2022-06-02 08:59:37 -0700

    Add files via upload

---
+++ src/main/java/us/java/target/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import main.UI.GUI;
+
+public class Main {
+    public static void main(String[] args) {
+        GUI.show();
+    }
+}
---
+++ src/main/java/us/java/target/UI/GUI.java
@@ -0,0 +1,65 @@
+package main.UI;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class GUI {
+    static private JPanel gui;
+    static private JFrame frame;
+    static private Crosshair crosshair;
+    static private JButton displayTargetButton;
+    static private JButton removeTargetButton;
+
+    public static void show(){
+        FlatDarkLaf.setup();
+        crosshair = new Crosshair();
+        gui = new JPanel(new GridLayout(0, 1, 10, 10));
+        gui.setPreferredSize(new Dimension(150,120));
+        gui.setBorder(new EmptyBorder(10, 10, 10, 10));
+        displayTargetButton = new JButton("Display target");
+        removeTargetButton = new JButton("Remove target");
+
+        displayTargetButton.addActionListener(e -> {
+            displayState();
+        });
+        removeTargetButton.addActionListener(e -> {
+            standbyState();
+        });
+
+        gui.add(displayTargetButton);
+        gui.add(removeTargetButton);
+
+        standbyState();
+        frame = new JFrame("DCS Waypoint Target");
+        frame.add(gui);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(true);
+        frame.setFocusableWindowState(true);
+        frame.pack();
+        frame.setResizable(false);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(0, dim.height-frame.getHeight());
+        frame.setVisible(true);
+    }
+
+    private static void displayState(){
+        crosshair.show();
+//        textArea.setText("Target displayed!");
+        displayTargetButton.setEnabled(false);
+        removeTargetButton.setEnabled(true);
+    }
+
+    private static void standbyState(){
+        crosshair.hide();
+//        textArea.setText("");
+        displayTargetButton.setEnabled(true);
+        removeTargetButton.setEnabled(false);
+    }
+}
